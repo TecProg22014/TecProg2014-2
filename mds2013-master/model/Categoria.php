32906c8dc0469040d8c8e1d69b15e90b20fb4122
Macário Soares <MacSoares@Mko-MacBook-Pro.local>
2014-08-25 16:40:15 -0300

    Adicionando comentários na classe Categoria

--- mds2013-master/model/Categoria.php
+++ mds2013-master/model/Categoria.php
@@ -2,9 +2,20 @@
 include_once('C:/xampp/htdocs/mds2013/exceptions/ETipoErrado.php');
 
 class Categoria{
+	
+	/**
+	 * Variables to specify the category of the delict
+	 * @var idCategoria
+	 * @var nomeCategoria
+	 */
 	private $idCategoria;
 	private $nomeCategoria;
 	
+	/**
+	 * Function to set the id of the category of the delict
+	 * @param int $idCategoria
+	 * @throws ETipoErrado
+	 */
 	public function __setIdCategoria($idCategoria){
 		
 		if (!is_numeric($idCategoria)){
@@ -12,9 +23,19 @@ class Categoria{
 		}
 		$this->idCategoria = $idCategoria;
 	}
+	
+	/**
+	 * Function to return the id of the category of the delict
+	 */
 	public function __getIdCategoria(){
 		return $this->idCategoria;
 	}
+	
+	/**
+	 * Function to set the name of the category of the delict
+	 * @param unknown $nomeCategoria
+	 * @throws ETipoErrado
+	 */
 	public function __setNomeCategoria($nomeCategoria){
 		
 		if(!is_string($nomeCategoria)){
@@ -22,14 +43,29 @@ class Categoria{
 		}
 		$this->nomeCategoria = $nomeCategoria;
 	}
+	
+	/**
+	 * Function to return the name of the category of the delict
+	 * @return Object Categoria var nomeCategoria 
+	 */
 	public function __getNomeCategoria(){
 		return $this->nomeCategoria;
 	}
+	
+	/**
+	 * Constructor to the object Categoria 
+	 * @param unknown $idCategoria
+	 * @param unknown $nomeCategoria
+	 */
 	public function __constructOverload($idCategoria,$nomeCategoria){
 		
 		$this->idCategoria = $idCategoria;
 		$this->nomeCategoria = $nomeCategoria;
 	}
+	
+	/**
+	 * Null constructor to grant that no null objects will be created
+	 */
 	public function __construct(){
 		
 	}
