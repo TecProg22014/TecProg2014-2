d0f74f0225af97f4669148a3b1aa45673a4d89ec
Tâmara Barbosa <tamara-bt@hotmail.com>
2014-08-27 07:42:38 -0300

    Adding coments into model Time

--- RadarCriminal/model/Tempo.php
+++ RadarCriminal/model/Tempo.php
@@ -1,11 +1,23 @@
 <?php
 include_once('C:/xampp/htdocs/mds2013/exceptions/ETipoErrado.php');
 class Tempo{
+	
+	/**
+	 * Variables that define the time of one crime
+	 * @var int idTempo
+	 * @var int intervalo
+	 * @var String mes
+	 */
+
 	private $idTempo;
 	private $intervalo;
 	private $mes;
 	
-	
+	/**
+	 * Function to set the id of the time of a crime
+	 * @param int $idTempo
+	 * @throws Exception ETipoErrado
+	 */
 	public function __setIdTempo($idTempo){
 		
 		if(!is_numeric($idTempo)){
@@ -13,9 +25,18 @@ class Tempo{
 		}
 		$this->idTempo = $idTempo;
 	}
+	/**
+	 * Function to get the id of a time of a crime
+	 * @return Object Time var $idTempo
+	 */
 	public function __getIdTempo(){
 		return $this->idTempo;
 	}
+	/**
+	 * Function to set the range of the time of a crime
+	 * @param int $intervalo
+	 * @throws Exception ETipoErrado
+	 */
 	public function __setIntervalo($intervalo){
 		
 		if(!is_numeric($intervalo)){
@@ -23,18 +44,40 @@ class Tempo{
 		}
 		$this->intervalo = $intervalo;
 	}
+	/**
+	 * Function to get the range of a time of a crime
+	 * @return Object Time var $intervalo
+	 */
 	public function __getIntervalo(){
 		return $this->intervalo;
 	}
+	/**
+	 * Function to set the name of a mes of a crime
+	 *@param String mes
+	 */
 	public function __setMes($mes){
 		$this->mes = $mes;
 	}
+	/**
+	 * Function to get the name of a mes of a crime
+	 * @return unknown
+	 */
 	public function __getMes(){
 		return  $this->mes;
 	}
+
+	/**
+	 * Null constructor to grant that no null objects will be created
+	 */
 	public function __construct(){
 		
 	}
+	/**
+	 * Full constructor of an object Time that defines a crime
+	 * @param int idTempo
+	 * @param int intervalo
+	 * @param String mes
+	 */
 	public function __constructOverload($idTempo,$intervalo,$mes){
 		
 		$this->idTempo = $idTempo;
