6edec740380a9c634dd6773f307157cbe4815eb1
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2014-08-30 19:31:32 -0300

    Insercao de comentarios para as classes de persistencia - autenticacao de sessao

--- SeboEletronico-master/SeboEletronicov2.0/Dao/autenticacaoUsuario.php
+++ SeboEletronico-master/SeboEletronicov2.0/Dao/autenticacaoUsuario.php
@@ -12,20 +12,25 @@ $sql2 = mysql_query("SELECT * FROM senha WHERE codigo_senha ='".$senha."'");
 $row = mysql_num_rows($sql);
 $row2 = mysql_num_rows($sql2);
 
+/**
+ * Obtendo dados para inicialização de sessão do usuário no sistema.
+ * Caso $email e $senha sejam compatíveis o login é efetuado com sucesso (janela window.location)
+ * */
+
 $usuario = mysql_fetch_array($sql);
 $idUsuario = $usuario['id_usuario'];
 if($row == $row2){
-    if($row>0){
-        session_start();
-        $_SESSION['email']= $email;
-        $_SESSION['senha']= $senha;
-        $_SESSION['id_usuario'] = $idUsuario;
-        //echo "<script>alert('Seja bem vindo ao SEBO Eletronico!')</script>";
-        echo"<script>window.location='http://localhost/SeboEletronicov2.0/Visao/indexLogin.php'</script>";
-    }
+	if($row>0){
+		session_start();
+		$_SESSION['email']= $email;
+		$_SESSION['senha']= $senha;
+		$_SESSION['id_usuario'] = $idUsuario;
+		//echo "<script>alert('Seja bem vindo ao SEBO Eletronico!')</script>";
+		echo"<script>window.location='http://localhost/SeboEletronicov2.0/Visao/indexLogin.php'</script>";
+	}
 }else{
-        echo "<script>alert('Email de usuario ou senha invalido, tente novamente!')</script>";
-        echo "<script>  window.location='http://localhost/SeboEletronicov2.0/Visao/entrarLogin.php'</script>"; 
-    }
+	echo "<script>alert('Email de usuario ou senha invalido, tente novamente!')</script>";
+	echo "<script>  window.location='http://localhost/SeboEletronicov2.0/Visao/entrarLogin.php'</script>";
+}
 ?>
 
