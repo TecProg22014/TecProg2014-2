453bd58d8bc1e39e62b64db9e9716f66ecd399a2
Tâmara Barbosa <tamara-bt@hotmail.com>
2014-08-30 11:51:53 -0300

    Adding coments in Conexao

--- RadarCriminal/persistence/Conexao.php
+++ RadarCriminal/persistence/Conexao.php
@@ -3,12 +3,31 @@ require_once('C:/xampp/htdocs/mds2013/libs/adodb/adodb.inc.php');
 require_once('C:/xampp/htdocs/mds2013/exceptions/EConexaoFalha.php');
 class Conexao{
 	
+	/**
+	 * Variables to specify the conection with the base
+	 * @var $banco;
+	 * @var $tipo_banco;
+	 * @var $servidor;
+	 * @var $usuario;
+	 * @var $senha;
+	 * @var $db;
+	 */
 	public $banco;
 	private $tipo_banco;
 	private $servidor;
 	private $usuario;
 	private $senha;
 	private $db;
+	
+	/**
+	 * Full constructor to the object Conexao
+	 * @param $banco;
+	 * @param $tipo_banco;
+	 * @param $servidor;
+	 * @param $usuario;
+	 * @param $senha;
+	 * @param $db;
+	 */
 	public function __construct(){
 		
 		$this->tipo_banco    = "mysql";
@@ -16,7 +35,7 @@ class Conexao{
 		$this->usuario       = "root";
 		$this->senha         = "";
 		$this->db            = "radar_criminal";
-		$this->banco = NewADOConnection($this->tipo_banco);
+		$this->banco 		 = NewADOConnection($this->tipo_banco);
 		$this->banco->dialect = 3;
 		$this->banco->debug = false;
 		$this->banco->Connect($this->servidor,$this->usuario,$this->senha,$this->db);
