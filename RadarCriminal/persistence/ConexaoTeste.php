c9243ae31c5816b7978240e021a36cc474050365
Tâmara Barbosa <tamara-bt@hotmail.com>
2014-08-31 15:23:40 -0300

    Inserting comments into ConexaoTeste'

--- RadarCriminal/persistence/ConexaoTeste.php
+++ RadarCriminal/persistence/ConexaoTeste.php
@@ -3,12 +3,31 @@ require_once('C:/xampp/htdocs/mds2013/libs/adodb/adodb.inc.php');
 require_once('C:/xampp/htdocs/mds2013/exceptions/EConexaoFalha.php');
 class ConexaoTeste{
 	
+	/**
+	 * Variables to specify the conection with the base and the tests
+	 * @var $banco;
+	 * @var $tipo_banco;
+	 * @var $servidor;
+	 * @var $usuario;
+	 * @var $senha;
+	 * @var $db;
+	 */
 	public $banco;
 	private $tipo_banco;
 	private $servidor;
 	private $usuario;
 	private $senha;
 	private $db;
+	
+	/**
+	 * Full constructor to the object ConexaoTeste
+	 * @param $banco;
+	 * @param $tipo_banco;
+	 * @param $servidor;
+	 * @param $usuario;
+	 * @param $senha;
+	 * @param $db;
+	 */
 	public function __construct(){
 		
 		$this->tipo_banco    = "mysql";
