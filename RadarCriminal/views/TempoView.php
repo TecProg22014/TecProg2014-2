89698c7289e70d785ee99de8fdec348380f9f028
Macário Soares <macario.junior@gmail.com>
2014-08-31 22:54:51 -0300

    Adding comments to time view class

--- RadarCriminal/views/TempoView.php
+++ RadarCriminal/views/TempoView.php
@@ -1,22 +1,31 @@
 <?php
-include_once('C:/xampp/htdocs/mds2013/controller/TempoController.php');
-class TempoView{
-        private $tempoCO;
-        public function __construct(){
-                $this->tempoCO = new TempoController();
-        }
-
-        public function retornarDadosTempoFormatado(){
-                return $this->tempoCO->_retornarDadosFormatados();
-        }
-        //Metodo duplicado para adaptacao do retorno de dados para a nova interface
-        /**
-         * @author Eduardo Augusto
-         * @author Sergio Silva
-         * @author Eliseu Egewarth
-         * @copyright RadarCriminal 2013
-         **/
-        public function retornarDadosTempoFormatoNovo(){
-                return $this->tempoCO->_retornarDadosFormatoNovo();
-        }
+include_once ('C:/xampp/htdocs/mds2013/controller/TempoController.php');
+class TempoView {
+	/**
+	 * Variable to instance an object of time controller
+	 * @var tempoCO
+	 */
+	private $tempoCO;
+	
+	/**
+	 * Constructor to instance an object of time controller
+	 */
+	public function __construct() {
+		$this->tempoCO = new TempoController ();
+	}
+	
+	/**
+	 * Function to get data of all periods of time organized in labels
+	 * @return String $dataInLabels
+	 */
+	public function retornarDadosTempoFormatado() {
+		return $this->tempoCO->_retornarDadosFormatados ();
+	}
+	
+	/**
+	 * Function to get data of periods of time organized in new labels to construct diferent graphs
+	 */
+	public function retornarDadosTempoFormatoNovo() {
+		return $this->tempoCO->_retornarDadosFormatoNovo ();
+	}
 }
