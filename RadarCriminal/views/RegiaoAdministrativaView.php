fa303199f72e8ff14529528a0fc71baf42089bbf
Macário Soares <macario.junior@gmail.com>
2014-08-31 23:01:33 -0300

    Adding comments to Administrative region view class

--- RadarCriminal/views/RegiaoAdministrativaView.php
+++ RadarCriminal/views/RegiaoAdministrativaView.php
@@ -1,21 +1,36 @@
 <?php
-include_once('C:/xampp/htdocs/mds2013/controller/RegiaoAdministrativaController.php');
-
-class RegiaoAdministrativaView{
+include_once ('C:/xampp/htdocs/mds2013/controller/RegiaoAdministrativaController.php');
+class RegiaoAdministrativaView {
+	/**
+	 * Variable to instance a new object of administrative region controller
+	 * @var raCO
+	 */
 	private $raCO;
-
-	public function __construct(){
-		$this->raCO = new RegiaoAdministrativaController();
+	
+	/**
+	 * Constructor to instance a new object of administrative region controller
+	 */
+	public function __construct() {
+		$this->raCO = new RegiaoAdministrativaController ();
 	}
-	public function listarTodasAlfabeticamente(){
-		$nomeRA = $this->raCO->_listarTodasAlfabeticamente();
-		for($i = 0, $retornoRA = ""; $i < count($nomeRA); $i++){
-			$retornoRA = $retornoRA."<li><a class=\"submenu\" href=\"?pag=u\"><i class=\"icon-map-marker\"></i><span class=\"hidden-tablet\">".$nomeRA[$i]."</span></a></li>";
+	
+	/**
+	 * unction to get alphabetically all the administrative regions
+	 * @return string $retornoRA
+	 */
+	public function listarTodasAlfabeticamente() {
+		$nomeRA = $this->raCO->_listarTodasAlfabeticamente ();
+		for($i = 0, $retornoRA = ""; $i < count ( $nomeRA ); $i ++) {
+			$retornoRA = $retornoRA . "<li><a class=\"submenu\" href=\"?pag=u\"><i class=\"icon-map-marker\"></i><span class=\"hidden-tablet\">" . $nomeRA [$i] . "</span></a></li>";
 		}
 		return $retornoRA;
 	}
 	
-	public function contarRegistrosRA(){
-		return $this->raCO->_contarRegistrosRA();
+	/**
+	 * Function to get the count of administrative region regists exist
+	 * @return int $countRA
+	 */
+	public function contarRegistrosRA() {
+		return $this->raCO->_contarRegistrosRA ();
 	}
 }
