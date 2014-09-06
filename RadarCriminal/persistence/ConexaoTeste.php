0fb4a28e4419ecbd128682893462e8e802c6da20
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2014-09-06 19:33:14 -0300

    Add path

--- RadarCriminal/persistence/ConexaoTeste.php
+++ RadarCriminal/persistence/ConexaoTeste.php
@@ -1,6 +1,8 @@
 <?php
-require_once('C:/xampp/htdocs/mds2013/libs/adodb/adodb.inc.php');
-require_once('C:/xampp/htdocs/mds2013/exceptions/EConexaoFalha.php');
+/** Returns the physical address of the web server */
+$SERVER_ADDRESS = $_SERVER['DOCUMENT_ROOT'];
+require_once $SERVER_ADDRESS."/TecProg2014-2/RadarCriminal/libs/adodb/adodb.inc.php";
+require_once $SERVER_ADDRESS."/TecProg2014-2/RadarCriminal/exceptions/EConexaoFalha.php";
 class ConexaoTeste{
 
 	/**
