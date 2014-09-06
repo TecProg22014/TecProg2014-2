0602bedb967bdd8dc052a54cbbb4ca03a7da3cb4
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2014-09-06 17:58:35 -0300

    Correction paths

--- RadarCriminal/index.php
+++ RadarCriminal/index.php
@@ -1,6 +1,8 @@
 <?php
-include './view/header.php';
-include './view/router.php';
-include './view/footer.php';
+/** Returns the physical address of the web server */
+$SERVER_ADDRESS = $_SERVER['DOCUMENT_ROOT'];
+include $SERVER_ADDRESS."/TecProg2014-2/RadarCriminal/front/header.php";
+include $SERVER_ADDRESS."/TecProg2014-2/RadarCriminal/front/router.php";
+include $SERVER_ADDRESS."/TecProg2014-2/RadarCriminal/front/footer.php";
 
 ?>
