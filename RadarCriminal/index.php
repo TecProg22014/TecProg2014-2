1f7de4722213adf7844b41c9f92958bb43f543c5
Eliseu Egewarth <eliseuegewarth@gmail.com>
2014-09-07 20:09:33 -0300

    Correcting Paths in include of index.php

--- RadarCriminal/index.php
+++ RadarCriminal/index.php
@@ -1,8 +1,8 @@
 <?php
-/** Returns the physical address of the web server */
-$SERVER_ADDRESS = $_SERVER['DOCUMENT_ROOT'];
-include $SERVER_ADDRESS."/TecProg2014-2/RadarCriminal/front/header.php";
-include $SERVER_ADDRESS."/TecProg2014-2/RadarCriminal/front/router.php";
-include $SERVER_ADDRESS."/TecProg2014-2/RadarCriminal/front/footer.php";
+
+
+include "/front/header.php";
+include "/front/router.php";
+include "/front/footer.php";
 
 ?>
