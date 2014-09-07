ec80e8e81755cf7f1e99855abb7826c40d4d332f
Eliseu Egewarth <eliseuegewarth@gmail.com>
2014-09-07 12:08:59 -0300

    Adding global constant  in RegiaoAdministrativaController.php

--- RadarCriminal/controller/RegiaoAdministrativaController.php
+++ RadarCriminal/controller/RegiaoAdministrativaController.php
@@ -1,4 +1,6 @@
 <?php
+/** Returns the physical address of the web server */
+$SERVER_ADDRESS = $_SERVER['DOCUMENT_ROOT'];
 include_once $SERVER_ADDRESS."/TecProg2014-2/RadarCriminal/persistence/RegiaoAdministrativaDAO.php";
 include_once $SERVER_ADDRESS."/TecProg2014-2/RadarCriminal/exceptions/EErroConsulta.php";
 include_once $SERVER_ADDRESS."/TecProg2014-2/RadarCriminal/model/RegiaoAdministrativa.php";
