e19c62a08c5ee68d2dc144b503cc05f0c02844ce
Tâmara Barbosa <tamara-bt@hotmail.com>
2014-09-11 01:25:15 -0300

    Correcting Paths in include of Controller tests

--- SeboEletronico/Teste/TesteControle/LivroControladorTest.php
+++ SeboEletronico/Teste/TesteControle/LivroControladorTest.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once "../../Modelo/Livro.php";
-require_once "../../Controle/LivroControlador.php";
-require_once "../../Dao/LivroDao.php";
-require_once "../../Utilidades/ValidaDados.php";
-require_once "../../Utilidades/ExcessaoNomeInvalido.php";
-require_once "../../Utilidades/ExcessaoTituloInvalido.php";
-require_once "../../Utilidades/ExcessaoEditoraInvalida.php";
-require_once "../../Utilidades/ConexaoComBanco.php";
+require_once "/Modelo/Livro.php";
+require_once "/Controle/LivroControlador.php";
+require_once "/Dao/LivroDao.php";
+require_once "/Utilidades/ValidaDados.php";
+require_once "/Utilidades/ExcessaoNomeInvalido.php";
+require_once "/Utilidades/ExcessaoTituloInvalido.php";
+require_once "/Utilidades/ExcessaoEditoraInvalida.php";
+require_once "/Utilidades/ConexaoComBanco.php";
 
 class LivroControladorTest extends PHPUnit_Framework_TestCase{
     
