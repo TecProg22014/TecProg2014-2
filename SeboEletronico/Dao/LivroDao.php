8782531ef29b220cbd76a3eee30dcb7d43e216b7
Tâmara Barbosa <tamara-bt@hotmail.com>
2014-09-16 13:00:32 -0300

    Ajusting complex functions in LivroDao

--- SeboEletronico/Dao/LivroDao.php
+++ SeboEletronico/Dao/LivroDao.php
@@ -10,37 +10,48 @@ class LivroDao {
 		/**
 		 * Insertion method whose return is an object
 		 * */
-		$sql = "INSERT INTO livro (id_dono, titulo_livro, editora, autor, edicao, genero, estado_conserv, descricao_livro, venda, troca)
-            VALUES ('".$id_dono."','".$livro->getTitulo()."','".$livro->getEditora()."','".$livro->getAutor()."',
-                '".$livro->getEdicao()."','".$livro->getGenero()."','".$livro->getEstado()."','".$livro->getDescricao()."','".$livro->getVenda()."',
-                    '".$livro->getTroca()."')";
+		$sql = "INSERT INTO livro (id_dono, titulo_livro, editora, autor, edicao,
+								   genero, estado_conserv, descricao_livro, venda, troca)
+                VALUES ('".$id_dono."','".$livro->getTitulo()."','".$livro->getEditora()."',
+                		'".$livro->getAutor()."','".$livro->getEdicao()."',
+                		'".$livro->getGenero()."','".$livro->getEstado()."',
+                		'".$livro->getDescricao()."','".$livro->getVenda()."',
+                		'".$livro->getTroca()."')";
+		
 		$livro = mysql_query($sql);
 		return $livro;
 	}
 
 	/**
-	 * Research methods book of the application, all variations of consulting books are concentrated in this method
+	 * Research methods book of the application, 
+	 * all variations of consulting books are concentrated in this method
 	 * Return method: Object
 	 * */
-
-	public function pesquisaLivro( $titulo, $estadoNovo, $estadoUsado, $disponibilidadeVenda, $disponibilidadeTroca ){
+	public function pesquisaLivro( $titulo, $estadoNovo, $estadoUsado, 
+								   $disponibilidadeVenda, $disponibilidadeTroca ){
 
 		if( empty( $disponibilidadeTroca ) && !empty( $disponibilidadeVenda )){
 			if( empty( $estadoNovo ) && !empty( $estadoUsado )){
-				$sql = "SELECT * FROM livro WHERE titulo_livro = '".$titulo."' AND estado_conserv = '".$estadoUsado."'
-            	AND tipo_operacao = '".$disponibilidadeVenda."'";
+				$sql = "SELECT * FROM livro WHERE titulo_livro = '".$titulo."' AND 
+								 estado_conserv = '".$estadoUsado."'
+            	                 AND tipo_operacao = '".$disponibilidadeVenda."'";
+				
 			} elseif( !empty( $estadoNovo ) && empty( $estadoUsado )) {
-				$sql = "SELECT * FROM livro WHERE titulo_livro = '".$titulo."' AND estado_conserv = '".$estadoNovo."'
-            AND tipo_operacao = '".$disponibilidadeVenda."'";
+				$sql = "SELECT * FROM livro WHERE titulo_livro = '".$titulo."' 
+						AND estado_conserv = '".$estadoNovo."'
+            			AND tipo_operacao = '".$disponibilidadeVenda."'";
 			}
 			
 		}else if( !empty( $disponibilidadeTroca ) && empty( $disponibilidadeVenda )){
 			if( empty( $estadoNovo ) && !empty( $estadoUsado )){
-				$sql = "SELECT * FROM livro WHERE titulo_livro = '".$titulo."' AND estado_conserv = '".$estadoUsado."'
-           	 	AND tipo_operacao = '".$disponibilidadeTroca."'";
+				$sql = "SELECT * FROM livro WHERE titulo_livro = '".$titulo."' 
+						AND estado_conserv = '".$estadoUsado."'
+           	 			AND tipo_operacao = '".$disponibilidadeTroca."'";
+				
 			} elseif( !empty( $estadoNovo ) && empty( $estadoUsado )) {
-				$sql = "SELECT * FROM livro WHERE titulo_livro = '".$titulo."' AND estado_conserv = '".$estadoNovo."'
-            	AND tipo_operacao = '".$disponibilidadeTroca."'";
+				$sql = "SELECT * FROM livro WHERE titulo_livro = '".$titulo."' 
+						AND estado_conserv = '".$estadoNovo."'
+            			AND tipo_operacao = '".$disponibilidadeTroca."'";
 			}
 		} else{
 			$sql = "SELECT * FROM livro WHERE titulo_livro = '".$titulo."'";
@@ -79,9 +90,17 @@ class LivroDao {
 	}
 
 	public function alteraLivro( $livro, $id_dono, $id_usuario ){
-		$sql = "UPDATE livro SET id_dono = '".$id_usuario."', titulo_livro = '".$livro->getTitulo()."', editora = '".$livro->getEditora()."',
-            	autor = '".$livro->getAutor()."', edicao = '".$livro->getEdicao()."', genero = '".$livro->getGenero()."', estado_conserv = '".$livro->getEstado()."', 
-                descricao_livro = '".$livro->getDescricao()."', venda = '".$livro->getVenda()."', troca = '".$livro->getTroca()."' WHERE id_livro = '".$id_dono."'";
+		$sql = "UPDATE livro SET id_dono = '".$id_usuario."', 
+				titulo_livro = '".$livro->getTitulo()."', 
+				editora = '".$livro->getEditora()."',
+            	autor = '".$livro->getAutor()."', 
+            	edicao = '".$livro->getEdicao()."',
+            	genero = '".$livro->getGenero()."', 
+            	estado_conserv = '".$livro->getEstado()."', 
+                descricao_livro = '".$livro->getDescricao()."', 
+                venda = '".$livro->getVenda()."', 
+                troca = '".$livro->getTroca()."' WHERE id_livro = '".$id_dono."'";
+		
 		$livro = mysql_query($sql);
 		return $livro;
 	}
