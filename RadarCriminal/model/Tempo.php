165eba294c473f57106bb9ede471a52ca8151b87
Tâmara Barbosa <tamara-bt@hotmail.com>
2014-09-18 11:28:02 -0300

    Fixing bugs

--- RadarCriminal/model/Tempo.php
+++ RadarCriminal/model/Tempo.php
@@ -28,8 +28,9 @@ class Tempo{
 		if(!is_numeric($idTempo)){
 			throw new ETipoErrado();
 		}else{
-			$this->idTempo = $idTempo;
+			//nothing to do here
 		}
+		$this->idTempo = $idTempo;
 	}
 	/**
 	 * Function to get the id of a time of a crime
@@ -48,8 +49,9 @@ class Tempo{
 		if(!is_numeric($intervalo)){
 			throw new ETipoErrado();
 		}else{
-			$this->intervalo = $intervalo;
+			//nothing to do here
 		}
+		$this->intervalo = $intervalo;
 	}
 	
 	/**
