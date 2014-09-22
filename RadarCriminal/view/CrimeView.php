a9063cdd91bc3b7dba76fbf6619057a3cfd0a122
Macário Soares <macario.junior@gmail.com>
2014-09-22 20:30:37 -0300

    Aplying new variables and methods names in CrimeView.php

--- RadarCriminal/view/CrimeView.php
+++ RadarCriminal/view/CrimeView.php
@@ -1,117 +1,117 @@
 <?php
 
 
-include_once "/controller/CrimeController.php";
+include_once "/controller/crimeControllerntroller.php";
 class CrimeView{
 	
 	/**
 	 * Variable to instance one object of crime controller
-	 * @var crimeCO
+	 * @var crimeController
 	 */
-	private $crimeCO;
+	private $crimeControllerntroller;
 
 	/**
 	 * Constructor to instance a new object of crime controller
 	 */
 	public function __construct(){
-		$this->crimeCO = new CrimeController();
+		$this->crimeController = new crimeControllerntroller();
 	}
 
 	/**
 	 * Function to get data of crimes that will be represented on a graph
 	 * @return String $dataGraph  *refactor
 	 */
-	public function retornarDadosCrimeSomadoFormatoNovo(){
-		return $this->crimeCO->_retornarDadosDeSomaFormatoNovo();
+	public function getCrimesData(){
+		return $this->crimeController->getCrimesData();
 	}
 
 	/**
 	 * Function to get the sum of all crimes per year
-	 * @param String $ano
+	 * @param String $year
 	 * @return int $SumOfCrimes  *refactor
 	 */
-	public function somaDeCrimePorAno($ano){
-		return $this->crimeCO->_somaDeCrimePorAno($ano);
+	public function sumCrimesPerYear($year){
+		return $this->crimeController->_sumCrimesPerYear($year);
 	}
 	
 	/**
 	 * Function to get the sum of crimes related with onte type of nature
-	 * @param string $natureza
+	 * @param string $nature
 	 * @return int $numberOfCrimes   *refactor
 	 */
-	public function somaDeCrimePorNatureza($natureza){
-		return $this->crimeCO->_somaDeCrimePorNatureza($natureza);
+	public function sumCrimesPerNature($nature){
+		return $this->crimeController->_sumCrimesByNature($nature);
 	}
 	
 	/**
 	 * Function to get the sum of crimes of all years
 	 * @return int $numberOfCrimes    *refactor
 	 **/
-	public function _somaCrimeTodosAnos(){
-		return $this->crimeCO->_somaCrimeTodosAnos();
+	public function _sumAllTimesCrimes(){
+		return $this->crimeController->_sumCrimesOfAllYears();
 	}
 
 	/**
 	 * Function to get the sum of how many crimes happened between 2010 and 2011
 	 * @return int $numberOfCrimes    *refactor
 	 */
-	public function _somaCrime2010_2011(){
-		return $this->crimeCO->_somaCrime2010_2011();
+	public function _sumCrimes2010_2011(){
+		return $this->crimeController->_sumCrimes2010_2011();
 	}
 	
 	/**
 	 * Function to get the sum of how many homicides happened between 2010 and 2011
 	 * @return int $numberOfCrimes    *refactor
 	 **/
-	public function _somaHomicidios2010_2011(){
-		return $this->crimeCO->_somaHomicidios2010_2011();
+	public function _sumHomicides2010_2011(){
+		return $this->crimeController->_sumHomicides2010_2011();
 	}
 
 	/**
 	 * Function to get the sum of all homicides 
 	 * @return int $numberOfCrimes   *refactor
 	 */
-	public function _somaTotalHomicidios(){
-		return $this->crimeCO->_somaTotalHomicidios();
+	public function _sumAllHomicides(){
+		return $this->crimeController->_sumAllHomicides();
 	}
 
 	/**
 	 * Function to get the sum of all injury
 	 * @return int $numberOfCrimes   *refactor
 	 */
-	public function _somaLesaoCorporal(){
-		return $this->crimeCO->_somaLesaoCorporal();
+	public function _sumAllInjury(){
+		return $this->crimeController->_sumAllInjury();
 	}
 
 	/**
 	 * Function to get the sum of all injury between 2010 and 2011
 	 * @return int $numberOfCrimes   *refactor
 	 */
-	public function _somaLesaoCorporal2010_2011(){
-		return $this->crimeCO->_somaLesaoCorporal2010_2011();
+	public function _sumInjury2010_2011(){
+		return $this->crimeController->_sumInjury2010_2011();
 	}
 
 	/**
 	 * Function to get the sum of all murder attempts
 	 * @return int $numberOfCrimes   *refactor
 	 */
-	public function _somaTotalTentativasHomicidio(){
-		return $this->crimeCO->_somaTotalTentativasHomicidio();
+	public function _sumAllMurderAttempts(){
+		return $this->crimeController->_sumAllMurderAttempts();
 	}
 
 	/**
 	 * Function to get the sum of all murder attempts between 2010 and 2011
 	 * @return int $numberOfCrimes   *refactor
 	 */
-	public function _somaTotalTentativasHomicidio2010_2011(){
-		return $this->crimeCO->_somaTotalTentativasHomicidio2010_2011();
+	public function _sumMurderAttepmts2010_2011(){
+		return $this->crimeController->_sumMurderAttepmts2010_2011();
 	}
 
 	/**
 	 * Function to get the sum of all crimes
 	 * @return int $numberOfCrimes   *refactor
 	 */
-	public function _somarGeral(){
-		return $this->crimeCO->_somarGeral();
+	public function _sumAllCrimes(){
+		return $this->crimeController->_sumAllCrimes();
 	}
 }
