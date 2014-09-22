74ee4bd427429d8b083dc339630fff367ac47720
Tâmara Barbosa <tamara-bt@hotmail.com>
2014-09-21 23:50:53 -0300

    Changing name of variables

--- RadarCriminal/persistence/ConexaoTeste.php
+++ RadarCriminal/persistence/ConexaoTeste.php
@@ -10,39 +10,39 @@ class ConexaoTeste{
 
 	/**
 	 * Variables to specify the conection with the base and the tests
-	 * @var $banco;
-	 * @var $tipo_banco;
-	 * @var $servidor;
-	 * @var $usuario;
-	 * @var $senha;
+	 * @var $base;
+	 * @var $baseType;
+	 * @var $server;
+	 * @var $user;
+	 * @var $key;
 	 * @var $db;
 	 */
-	public $banco;
-	private $tipo_banco;
-	private $servidor;
-	private $usuario;
-	private $senha;
+	public $base;
+	private $baseType;
+	private $server;
+	private $user;
+	private $key;
 	private $db;
 
 	/**
 	 * Full constructor to the object ConexaoTeste
-	 * @param $banco;
-	 * @param $tipo_banco;
-	 * @param $servidor;
-	 * @param $usuario;
-	 * @param $senha;
+	 * @param $base;
+	 * @param $baseType;
+	 * @param $server;
+	 * @param $user;
+	 * @param $key;
 	 * @param $db;
 	 */
 	public function __construct( ){
 
-		$this->tipo_banco    = "mysql";
-		$this->servidor      = "localhost";
-		$this->usuario       = "root";
-		$this->senha         = "";
+		$this->baseType      = "mysql";
+		$this->server        = "localhost";
+		$this->user        	 = "root";
+		$this->key           = "";
 		$this->db            = "radar_criminal_teste";
-		$this->banco = NewADOConnection($this->tipo_banco);
-		$this->banco->dialect = 3;
-		$this->banco->debug = false;
-		$this->banco->Connect($this->servidor,$this->usuario,$this->senha,$this->db);
+		$this->base = NewADOConnection($this->baseType);
+		$this->base->dialect = 3;
+		$this->base->debug = false;
+		$this->base->Connect($this->server,$this->user,$this->key,$this->db);
 	}
 }
