9ed5f123a431a1a813fdf2e231a06d44e6157895
Tâmara Barbosa <tamara-bt@hotmail.com>
2014-10-03 10:59:27 -0300

    Aplying html stylesheet

--- RadarCriminal/view/footer.php
+++ RadarCriminal/view/footer.php
@@ -1,71 +1,62 @@
-				
-		<div class="modal hide fade" id="myModal">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal">×</button>
-				<h3>Configurações</h3>
-			</div>
-			<div class="modal-body">
-				<p>#Configurações aqui...</p>
-			</div>
-			<div class="modal-footer">
-				<a href="#" class="btn" data-dismiss="modal">Fechar</a>
-				<a href="#" class="btn btn-primary">Salvar</a>
-			</div>
-		</div>
-		
-		<div class="clearfix"></div>
-		
-		<footer>
-			<p>
-				<span style="text-align:left;float:left">&copy; 2013 <a href="http://www.unb.br" alt="UnB">UnB - Universidade de Brasília</a></span>
-				<span class="hidden-phone" style="text-align:right;float:right">MDS: <a href="https://matriculaweb.unb.br/matriculaweb/graduacao/disciplina.aspx?cod=193640" alt="MDS - 193640">Métodos de Desenvolvimento de Software</a></span>
-			</p>
+<div class="modal hide fade" id="myModal">
+   <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal">×</button>
+      <h3>Configurações</h3>
+   </div>
+   <div class="modal-body">
+      <p>#Configurações aqui...</p>
+   </div>
+   <div class="modal-footer">
+      <a href="#" class="btn" data-dismiss="modal">Fechar</a>
+      <a href="#" class="btn btn-primary">Salvar</a>
+   </div>
+</div>
+<div class="clearfix"></div>
+<footer>
+   <p>
+      <span style="text-align:left;float:left">&copy; 2013 <a href="http://www.unb.br" alt="UnB">UnB - Universidade de Brasília</a></span>
+      <span class="hidden-phone" style="text-align:right;float:right">MDS: <a href="https://matriculaweb.unb.br/matriculaweb/graduacao/disciplina.aspx?cod=193640" alt="MDS - 193640">Métodos de Desenvolvimento de Software</a></span>
+   </p>
+</footer>
 
-		</footer>
-				
-	</div><!--/.fluid-container-->
+<script src="./front/js/jquery-1.10.2.min.js"></script>
+<script src="./front/js/jquery-migrate-1.2.1.min.js"></script>	
+<script src="./front/js/jquery-ui-1.10.3.custom.min.js"></script>	
+<script src="./front/js/jquery.ui.touch-punch.js"></script>	
+<script src="./front/js/modernizr.js"></script>	
+<script src="./front/js/bootstrap.min.js"></script>	
+<script src="./front/js/jquery.cookie.js"></script>	
+<script src='./front/js/fullcalendar.min.js'></script>	
+<script src='./front/js/jquery.dataTables.min.js'></script>
+<script src="./front/js/excanvas.js"></script>
+<script src="./front/js/jquery.flot.js"></script>
+<script src="./front/js/jquery.flot.pie.js"></script>
+<script src="./front/js/jquery.flot.stack.js"></script>
+<script src="./front/js/jquery.flot.resize.min.js"></script>
+<script src="./front/js/jquery.flot.time.js"></script>
+<script src="./front/js/jquery.chosen.min.js"></script>	
+<script src="./front/js/jquery.uniform.min.js"></script>		
+<script src="./front/js/jquery.cleditor.min.js"></script>	
+<script src="./front/js/jquery.noty.js"></script>	
+<script src="./front/js/jquery.elfinder.min.js"></script>	
+<script src="./front/js/jquery.raty.min.js"></script>	
+<script src="./front/js/jquery.iphone.toggle.js"></script>	
+<script src="./front/js/jquery.uploadify-3.1.min.js"></script>	
+<script src="./front/js/jquery.gritter.min.js"></script>	
+<script src="./front/js/jquery.imagesloaded.js"></script>	
+<script src="./front/js/jquery.masonry.min.js"></script>	
+<script src="./front/js/jquery.knob.modified.js"></script>	
+<script src="./front/js/jquery.sparkline.min.js"></script>	
+<script src="./front/js/counter.min.js"></script>	
+<script src="./front/js/raphael.2.1.0.min.js"></script>
+<script src="./front/js/justgage.1.0.1.min.js"></script>	
+<script src="./front/js/jquery.autosize.min.js"></script>	
+<script src="./front/js/retina.js"></script>
+<script src="./front/js/jquery.placeholder.min.js"></script>
+<script src="./front/js/wizard.min.js"></script>
+<script src="./front/js/core.js"></script>	
+<script src="./front/js/charts.js"></script>	
+<script src="./front/js/custom.js"></script>
 
-	<!-- start: JavaScript-->
-		<script src="./front/js/jquery-1.10.2.min.js"></script>
-	<script src="./front/js/jquery-migrate-1.2.1.min.js"></script>	
-		<script src="./front/js/jquery-ui-1.10.3.custom.min.js"></script>	
-		<script src="./front/js/jquery.ui.touch-punch.js"></script>	
-		<script src="./front/js/modernizr.js"></script>	
-		<script src="./front/js/bootstrap.min.js"></script>	
-		<script src="./front/js/jquery.cookie.js"></script>	
-		<script src='./front/js/fullcalendar.min.js'></script>	
-		<script src='./front/js/jquery.dataTables.min.js'></script>
-		<script src="./front/js/excanvas.js"></script>
-	<script src="./front/js/jquery.flot.js"></script>
-	<script src="./front/js/jquery.flot.pie.js"></script>
-	<script src="./front/js/jquery.flot.stack.js"></script>
-	<script src="./front/js/jquery.flot.resize.min.js"></script>
-	<script src="./front/js/jquery.flot.time.js"></script>
-		
-		<script src="./front/js/jquery.chosen.min.js"></script>	
-		<script src="./front/js/jquery.uniform.min.js"></script>		
-		<script src="./front/js/jquery.cleditor.min.js"></script>	
-		<script src="./front/js/jquery.noty.js"></script>	
-		<script src="./front/js/jquery.elfinder.min.js"></script>	
-		<script src="./front/js/jquery.raty.min.js"></script>	
-		<script src="./front/js/jquery.iphone.toggle.js"></script>	
-		<script src="./front/js/jquery.uploadify-3.1.min.js"></script>	
-		<script src="./front/js/jquery.gritter.min.js"></script>	
-		<script src="./front/js/jquery.imagesloaded.js"></script>	
-		<script src="./front/js/jquery.masonry.min.js"></script>	
-		<script src="./front/js/jquery.knob.modified.js"></script>	
-		<script src="./front/js/jquery.sparkline.min.js"></script>	
-		<script src="./front/js/counter.min.js"></script>	
-		<script src="./front/js/raphael.2.1.0.min.js"></script>
-	<script src="./front/js/justgage.1.0.1.min.js"></script>	
-		<script src="./front/js/jquery.autosize.min.js"></script>	
-		<script src="./front/js/retina.js"></script>
-		<script src="./front/js/jquery.placeholder.min.js"></script>
-		<script src="./front/js/wizard.min.js"></script>
-		<script src="./front/js/core.js"></script>	
-		<script src="./front/js/charts.js"></script>	
-		<script src="./front/js/custom.js"></script>
-	<!-- end: JavaScript-->
-	
 </body>
 </html>
