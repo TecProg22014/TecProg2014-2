5d3395d1a5ec973204549b100d4f6cfaadf4919c
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2014-10-06 00:30:18 -0300

    Applying_Stylesheet--Visao #22

--- SeboEletronico/Visao/livrosDisponiveis.php
+++ SeboEletronico/Visao/livrosDisponiveis.php
@@ -2,9 +2,9 @@
 	session_start();
 	include '../Controle/LivroControlador.php';
 	$id = $_SESSION['id_usuario'];
-	$listaLivros = LivroControlador::getAllLivro();
+	$bookArray = LivroControlador::getAllBook();
 ?>
-
+<!DOCTYPE HTML>
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -44,12 +44,12 @@
 					<h5>Livros Dispon&iacute;veis</h5>
 				</th>
 			</tr>
-			<?php foreach($listaLivros as $chave => $valor){ ?>
+			<?php foreach($bookArrays as $bookId => $indexOfBook){ ?>
 			<tr>
 				<td>
 					<h2> T&iacute;tulo: </h2>
 					<h6>
-						<a href="http://localhost/TecProg2014-2/SeboEletronico/Visao/detalhesLivro.php?id_livro=<?php echo $valor['id_livro'] ?>"> <?php echo $valor['titulo_livro'] ?></a>
+						<a href="http://localhost/TecProg2014-2/SeboEletronico/Visao/detalhesLivro.php?id_livro=<?php echo $indexOfBook['id_livro'] ?>"> <?php echo $indexOfBook['titulo_livro'] ?></a>
 					</h6>
 				</td>
 			</tr>
