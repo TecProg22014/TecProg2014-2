4053b9c5ac5367c11f375480b66d5122aaaaf6e8
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2014-10-06 00:30:34 -0300

    Applying_Stylesheet--Visao #22

--- SeboEletronico/Visao/meusLivros.php
+++ SeboEletronico/Visao/meusLivros.php
@@ -1,10 +1,10 @@
 <?php
 	session_start();
 	include '../Controle/LivroControlador.php';
-	$id = $_SESSION['id_usuario'];
-	$listaLivros = LivroControlador::getLivroByIdUsuario( $id );
+	$bookIdAuthentication = $_SESSION['id_usuario'];
+	$bookArray = LivroControlador::getBookByUserId( $bookIdAuthentication );
 ?>
-
+<!DOCTYPE HTML>
 <html>
 	<head>	
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -45,14 +45,14 @@
 					<h5>Meus Livros</h5>
 				</th>
 			</tr>
-			<?php foreach($listaLivros as $chave => $valor){ ?>
+			<?php foreach($bookArrays as $bookId => $indexOfBook){ ?>
 
 			<tr>
 				<td>
 					<h2> T&iacute;tulo: </h2>
 					<h6>
 						<?php
-							echo $valor['titulo_livro'];
+							echo $indexOfBook['titulo_livro'];
 						?>
 					</h6>
 				</td>
@@ -61,7 +61,7 @@
 					<h2> Autor:</h2>
 					<h6>
 						<?php
-							echo $valor['autor'];
+							echo $indexOfBook['autor'];
 						?>
 					</h6>
 				</td>
@@ -70,7 +70,7 @@
 					<h2> Editora: </h2>
 					<h6>
 						<?php
-							echo $valor['editora'];
+							echo $indexOfBook['editora'];
 						?>
 					</h6>
 				</td>
@@ -79,7 +79,7 @@
 					<h2> Edi&ccedil;&atilde;o:</h2> 
 					<h6>
 						<?php
-							echo $valor['edicao'];
+							echo $indexOfBook['edicao'];
 						?>
 					</h6>
 				</td>
@@ -88,7 +88,7 @@
 					<h2> Descri&ccedil;&atilde;o: </h2>
 					<h6>
 						<?php
-							echo $valor['descricao_livro'];
+							echo $indexOfBook['descricao_livro'];
 						?>
 					</h6>
 				</td>
@@ -97,9 +97,9 @@
 					<h2> Tipo(s) de opera&ccedil;&atilde;o: </h2>
 					<h6>
 						<?php
-							echo $valor['venda'];
+							echo $indexOfBook['venda'];
 							echo "<br/>";
-							echo $valor['troca'];
+							echo $indexOfBook['troca'];
 						?>
 					</h6>
 				</td>
@@ -108,7 +108,7 @@
 					<h2> Genero: </h2>
 					<h6>
 						<?php
-							echo $valor['genero']
+							echo $indexOfBook['genero']
 						?>
 					</h6>
 				</td>
@@ -117,14 +117,14 @@
 					<h2> Estado:<h2/> 
 					<h6>
 						<?php
-							echo $valor['estado_conserv']
+							echo $indexOfBook['estado_conserv']
 						?>
 					</h6>
 				</td>
 					
 				<td>
-					<a href="http://localhost/TecProg2014-2/SeboEletronico/Visao/alterarLivro.php?id=<?php echo $valor['id_livro'] ?> " title="Alterar Livro"> <img src="img/icone_lapis.png" align="left"> </a>
-					<a href="http://localhost/TecProg2014-2/SeboEletronico/Utilidades/RecebeFormLivro.php?id_livro=<?php echo $valor['id_livro'] ?> " title="Excluir Livro"> <img src="img/icone_lixeira.png" align="right" > </a>
+					<a href="http://localhost/TecProg2014-2/SeboEletronico/Visao/alterarLivro.php?id=<?php echo $indexOfBook['id_livro'] ?> " title="Alterar Livro"> <img src="img/icone_lapis.png" align="left"> </a>
+					<a href="http://localhost/TecProg2014-2/SeboEletronico/Utilidades/RecebeFormLivro.php?id_livro=<?php echo $indexOfBook['id_livro'] ?> " title="Excluir Livro"> <img src="img/icone_lixeira.png" align="right" > </a>
 				</td>
 			</tr>
 			<?php }?>
