793fdee1a3889535d6796303d2f94e461e95a643
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2014-10-14 23:00:06 -0300

    Call Class Header into all files -- alterarLivro.php

--- SeboEletronico/Visao/alterarLivro.php
+++ SeboEletronico/Visao/alterarLivro.php
@@ -6,43 +6,12 @@
 	include '../Controle/LivroControlador.php';
 	$bookIdAuthentication = $_REQUEST['id'];
 	$bookArray = LivroControlador::getBookById( $id );
+	
+	include_once "Header.php";
+	$typeOfPage = "bookPage";
+	Header::loadMenuOfPage( $typeOfPage );
 
 ?>
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<link rel="stylesheet" href="http://localhost/TecProg2014-2/SeboEletronico/Visao/css/UsuarioStyle.css" type="text/css" media="all">
-		<link rel="stylesheet" href="http://localhost/TecProg2014-2/SeboEletronico/Visao/css/main.css" type="text/css" media="all">
-		<link rel="shortcut icon" href="http://localhost/TecProg2014-2/SeboEletronico/Visao/img/android.ico">
-		<script	src="http://localhost/TecProg2014-2/SeboEletronico/Utilidades/Redireciona.js"></script>
-		<title>Sebo Eletr&ocirc;nico</title>
-	</head>
-	<body>
-		<div id="header">
-			<div id="logo">
-				<img src="http://localhost/TecProg2014-2/SeboEletronico/Visao/img/sebo_header.png" class="imgHeader" />
-			</div>
-		</div>
-
-		<div id="mainmenu">
-			<button class="button" onclick="home()">Home</button>
-			<button class="button" onclick="user();">Usu&aacute;rio</button>
-			<button class="button" onclick="livro();">Livro</button>
-			<button class="button" onclick="sair();">Sair</button>
-		</div>
-
-		<div id="mainmenu">
-			<button class="button" onclick="meusLivros();">Meus Livros</button>
-			<button class="button" onclick="livrosDisponiveis();">Livros Dispon&iacute;veis</button>
-			<button class="button" onclick="cadastraLivro();">Cadastrar</button>
-			<button class="button" onclick="pesquisaLivro();">Pesquisar</button>
-		</div>
-
-		<br />
-		<br />
-		<br />
-
 
 		<form name="Insere Dados" action="http://localhost/TecProg2014-2/SeboEletronico/Utilidades/RecebeFormLivro.php" method="post" class="Formulario">
 			<table class='insr'>
