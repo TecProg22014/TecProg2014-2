c37eff05b16658d84dd1ff854a2f5b10d29d94ac
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2014-10-14 22:58:01 -0300

    Call Class Header into all files -- cadastrarUsuario.php

--- SeboEletronico/Visao/cadastrarUsuario.php
+++ SeboEletronico/Visao/cadastrarUsuario.php
@@ -1,31 +1,11 @@
 <?php
 	session_start();
 	$userIdAuthentication = $_SESSION['id_usuario'];
-?>
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<link rel="stylesheet" href="http://localhost/TecProg2014-2/SeboEletronico/Visao/css/UsuarioStyle.css" type="text/css" media="all">
-		<link rel="stylesheet" href="http://localhost/TecProg2014-2/SeboEletronico/Visao/css/main.css" type="text/css" media="all">
-		<link rel="shortcut icon" href="http://localhost/TecProg2014-2/SeboEletronico/Visao/img/android.ico">
-		<script src="http://localhost/TecProg2014-2/SeboEletronico/Utilidades/Redireciona.js"></script> 
-		<title>Sebo Eletr&ocirc;nico</title>
-	</head>
 	
-	<body>
-		<div id="header">
-			<div id="logo">
-				<img src="http://localhost/TecProg2014-2/SeboEletronico/Visao/img/sebo_header.png" class="imgHeader"/>
-			</div>
-		</div>
-		
-		<div id="mainmenu">
-			<button class="button" onclick="home();">Home</button>
-			<button class="button" onclick="login();">Login</button>
-		</div>
-		<br/><br/><br/>
-		
+	include_once "Header.php";
+	$typeOfPage = "userPage";
+	Header::loadMenuOfPage( $typeOfPage );
+?>
 		<form name="Insere Dados" action="http://localhost/TecProg2014-2/SeboEletronico/Utilidades/RecebeForm.php" method="post" class="formu">
 			<table class='insr'>
 				<tr>
