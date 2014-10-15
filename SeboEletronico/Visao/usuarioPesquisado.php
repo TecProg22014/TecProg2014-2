08d97b39adfa806d6ecfa53b88a1a6997e7032c2
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2014-10-14 23:06:05 -0300

    Call Class Header into all files -- usuarioPesquisado.php

--- SeboEletronico/Visao/usuarioPesquisado.php
+++ SeboEletronico/Visao/usuarioPesquisado.php
@@ -5,39 +5,11 @@
 	include '../Controle/UsuarioControlador.php';
 	$fullUserName = $_REQUEST['nome'];
 	$resultSearchUserByName = UsuarioControlador::pesquisaUsuario( $fullUserName );
+	
+	include_once "Header.php";
+	$typeOfPage = "userPage";
+	Header::loadMenuOfPage( $typeOfPage );
 ?>
-<!DOCTYPE HTML>
-<html>
-	<head>	
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<link rel="stylesheet" href="http://localhost/TecProg2014-2/SeboEletronico/Visao/css/UsuarioStyle.css" type="text/css" media="all">
-		<link rel="stylesheet" href="http://localhost/TecProg2014-2/SeboEletronico/Visao/css/main.css" type="text/css" media="all">
-		<link rel="shortcut icon" href="http://localhost/TecProg2014-2/SeboEletronico/Visao/img/android.ico">
-		<script src="http://localhost/TecProg2014-2/SeboEletronico/Utilidades/Redireciona.js"></script> 
-		<title>Sebo Eletr&ocirc;nico</title>
-	</head>
-
-	<body>
-		<div id="header">
-			<div id="logo">
-				<img src="http://localhost/TecProg2014-2/SeboEletronico/Visao/img/sebo_header.png" class="imgHeader"/>
-			</div>
-		</div>
-		
-		<div id="mainmenu">
-			<button class="button" onclick="home()">Home</button>
-			<button class="button" onclick="user();">Usu&aacute;rio</button>		
-			<button class="button" onclick="livro();">Livro</button>
-			<button class="button" onclick="sair();">Sair</button>
-		</div>
-
-		<div id="mainmenu">
-			<button class="button" onclick="altera();">Alterar</button>		
-			<button class="button" onclick="deleta();">Deletar</button> 
-			<button class="button" onclick="pesquisa();">Pesquisar</button>
-		</div>
-		
-		<br/><br/><br/>
 		
 		<table class='insr'>
 			<tr>
