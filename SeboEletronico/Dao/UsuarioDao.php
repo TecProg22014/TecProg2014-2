a64d80c81bbac44cb650334471ccdd0fa87b02c9
Sérgio Bezerra da Silva <mrbreakbeat@gmail.com>
2014-10-16 15:49:48 -0300

    Breaking into atomic functions -- UsuarioDao.php

--- SeboEletronico/Dao/UsuarioDao.php
+++ SeboEletronico/Dao/UsuarioDao.php
@@ -15,19 +15,23 @@ class UsuarioDao {
 	public function insertUser( $usuario ){
 		$auxiliaryPassword = $usuario->getUserPassword();
 		$finalPassword = $auxiliaryPassword[0];
-		 
-		$insertUserPassword = "INSERT INTO senha (codigo_senha) VALUES ('".$finalPassword."')";
-		mysql_query( $insertUserPassword );
-
-		$searchUserByPassword = "SELECT id_senha FROM senha WHERE codigo_senha='".$finalPassword."'";
-		$resultSearchUserByPassword = $passwordId = mysql_query( $searchUserByPassword );
-		$passwordId = mysql_fetch_array( $resultSearchUserByPassword );
+		$passwordId = insertPassword($auxiliaryPassword[0]);
 		$insertUser = "INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario, senha_usuario) VALUES ('".$usuario->getFullName()."', '".$usuario->getUserEmail()."',
 				'".$usuario->getUserPhoneNumber()."','".$passwordId['id_senha']."')";
 		$returnInsertUser = mysql_query( $insertUser );
 
-
 		return $returnInsertUser;
+	}
+	
+	public function insertPassword( $password ){
+		$insertUserPassword = "INSERT INTO senha (codigo_senha) VALUES ('".$password."')";
+		mysql_query( $insertUserPassword );
+		$searchUserByPassword = "SELECT id_senha FROM senha WHERE codigo_senha='".$finalPassword."'";
+		//#REFACTOR: Verify double iquals of variable resultSearchUserByPassword and $passwordId in next line
+		$resultSearchUserByPassword = $passwordId = mysql_query( $searchUserByPassword );
+		$passwordId = mysql_fetch_array( $resultSearchUserByPassword );
+		
+		return $passwordId;
 	}
 
 	public function updateUser( $usuario, $userId, $lastPassword ){
@@ -50,24 +54,6 @@ class UsuarioDao {
 		}
 	}
 
-	public function updateUser( $usuario, $userId, $lastPassword ){
-		$auxiliaryPassword = $usuario->getPassword();
-		$passwordModified = $auxiliaryPassword[0];
-		$updatePassword = "UPDATE usuario SET nome_usuario = '".$usuario->getFullName()."' , telefone_usuario = '".$usuario->getUserPhoneNumber()."',
-				           email_usuario = '".$usuario->getUserEmail()."' WHERE id_usuario = '".$userId."'";
-		$returnUpdatePassword = mysql_query( $updatePassword );
-
-		if( $passwordModified != $lastPassword ){
-			$searchLastPasswordForUser = "SELECT id_senha FROM senha WHERE codigo_senha='".$lastPassword."'";
-			$resultSearchLastPasswordForUser = mysql_query( $searchLastPasswordForUser );
-			$passwordId = mysql_fetch_row( $resultSearchLastPasswordForUser );
-			$updatePassword = "UPDATE senha SET codigo_senha = '".$passwordModified."' WHERE id_senha = '".$passwordId[0]."'";
-			$resultUpdatePassword = mysql_query( $updatePassword );
-		} else{
-			return ( $usuario && $updatePassword );
-		}
-	}
-
 	public function searchUser( $usuario ){
 		$searchUserByUserArray = "SELECT * FROM usuario WHERE nome_usuario = '".$usuario."'";
 		$resultSearchUserByUserArray = mysql_query( $searchUserByUserArray );
@@ -78,6 +64,7 @@ class UsuarioDao {
 	public function deleteUser( $userEmail, $userPassword ){
 		$deleteUserByEmail = "DELETE FROM usuario WHERE email_usuario = '".$userEmail."'";
 		$resultDeleteUserByEmail = mysql_query( $deleteUserByEmail );
+		//#REFACTOR: Implements on database CASCADE on DELETE on table 'senha'
 		$deleteUserByPassword = "DELETE FROM senha WHERE codigo_senha = '".$userPassword."'";
 		$resultDeleteUserByPassword = mysql_query( $deleteUserByPassword );
 		return ( $resultDeleteUserByEmail && $resultDeleteUserByPassword);
