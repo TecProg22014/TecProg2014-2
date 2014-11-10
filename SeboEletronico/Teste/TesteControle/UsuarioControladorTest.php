f8798b8571df60a4a97f8eb7195b96b3350e17d5
Macário Soares <macario.junior@gmail.com>
2014-11-10 08:48:10 -0200

    Ajusting include paths and instances to execute unit tests

--- SeboEletronico/Teste/TesteControle/UsuarioControladorTest.php
+++ SeboEletronico/Teste/TesteControle/UsuarioControladorTest.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once "/Modelo/Usuario.php";
-require_once "/Controle/UsuarioControlador.php";
-require_once "/Dao/UsuarioDao.php";
-require_once "/Utilidades/ValidaDados.php";
-require_once "/Utilidades/ExcessaoNomeInvalido.php";
-require_once "/Utilidades/ExcessaoSenhaInvalida.php";
-require_once "/Utilidades/ExcessaoTelefoneInvalido.php";
-require_once "/Utilidades/ExcessaoEmailInvalido.php";
-require_once "/Utilidades/ConexaoComBanco.php";
+require_once "../../Modelo/Usuario.php";
+require_once "../../Controle/UsuarioControlador.php";
+require_once "../../Dao/UsuarioDao.php";
+require_once "../../Utilidades/ValidaDados.php";
+require_once "../../Utilidades/ExcessaoNomeInvalido.php";
+require_once "../../Utilidades/ExcessaoSenhaInvalida.php";
+require_once "../../Utilidades/ExcessaoTelefoneInvalido.php";
+require_once "../../Utilidades/ExcessaoEmailInvalido.php";
+require_once "../../Utilidades/ConexaoComBanco.php";
 
 class UsuarioControladorTest extends PHPUnit_Framework_TestCase {
 
@@ -17,7 +17,7 @@ class UsuarioControladorTest extends PHPUnit_Framework_TestCase {
     
     protected function setUp() {
         $senha = array(123123,123123);
-        $this->usuario = new Usuario('lucas', 98989898, 'lucas@lucas.com', $senha);
+        $this->usuario = new User('lucas', 98989898, 'lucas@lucas.com', $senha);
         $this->usuarioControladorTest = new UsuarioControlador();
     }
 
@@ -32,44 +32,41 @@ class UsuarioControladorTest extends PHPUnit_Framework_TestCase {
         $this->assertTrue($retorno);
     }
     
-//    public function testChecaCadastroId(){
-//            
-//    }
-//    
-//    public function testAlteraUsuario(){
-//        $retorno = $this->usuarioControladorTest->alteraUsuario($this->usuario, 11, 123456);
-//        $this->assertTrue($retorno);
-//    }
-//    
-//    public function testPesquisaUsuario(){
-//        $retorno = $this->usuarioControladorTest->pesquisaUsuario(null);
-//        $this->assertFalse($retorno);
-//    }
-//    
-//    public function testDeletaUsuario(){
-//        $senha = $this->usuario->getSenha();
-//        $retorno = $this->usuarioControladorTest->deletaUsuario($this->usuario->getEmail(), $senha[0]);
-//        $this->assertTrue($retorno);
-//    }
-//    
-//    public function testGetCadastradosPorIdComIdNulo(){
-//        $retorno = $this->usuarioControladorTest->getCadastradosPorId(null);
-//        $this->assertFalse($retorno);
-//    }
-//    
-//    public function testGetCadastradosPorIdComIdInvalido(){
-//        $retorno = $this->usuarioControladorTest->getCadastradosPorId(-2);
-//        $this->assertFalse($retorno);
-//    }
-//    
-//    public function testGetCadastradosPorIdComIdValido(){
-//        $senha = $this->usuario->getSenha();
-//        $retorno = $this->usuarioControladorTest->getCadastradosPorId(23);
-//        $this->assertEquals($this->usuario->getEmail(), $retorno[4]);
-//        $this->assertEquals($this->usuario->getNome(), $retorno[1]);
-//        $this->assertEquals($this->usuario->getTelefone(), $retorno[3]);
-//
-//    }
+
+   public function testAlteraUsuario(){
+       $retorno = $this->usuarioControladorTest->alteraUsuario($this->usuario, 11, 123456);
+       $this->assertTrue($retorno);
+   }
+   
+   public function testPesquisaUsuario(){
+       $retorno = $this->usuarioControladorTest->pesquisaUsuario(null);
+       $this->assertFalse($retorno);
+   }
+   
+   public function testDeletaUsuario(){
+       $senha = $this->usuario->getSenha();
+       $retorno = $this->usuarioControladorTest->deletaUsuario($this->usuario->getEmail(), $senha[0]);
+       $this->assertTrue($retorno);
+   }
+   
+   public function testGetCadastradosPorIdComIdNulo(){
+       $retorno = $this->usuarioControladorTest->getCadastradosPorId(null);
+       $this->assertFalse($retorno);
+   }
+   
+   public function testGetCadastradosPorIdComIdInvalido(){
+       $retorno = $this->usuarioControladorTest->getCadastradosPorId(-2);
+       $this->assertFalse($retorno);
+   }
+   
+   public function testGetCadastradosPorIdComIdValido(){
+       $senha = $this->usuario->getSenha();
+       $retorno = $this->usuarioControladorTest->getCadastradosPorId(23);
+       $this->assertEquals($this->usuario->getEmail(), $retorno[4]);
+       $this->assertEquals($this->usuario->getNome(), $retorno[1]);
+       $this->assertEquals($this->usuario->getTelefone(), $retorno[3]);
+
+   }
 
 }
 
